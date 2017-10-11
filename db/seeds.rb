c873fd61774af4234dc521977c9dd4e57058cad3
Carlos Gonzalez <carlosdavid@gonzalez-abraham.com.mx>
2017-10-11 16:20:39 -0500

    Initial seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+boards = Board.create(
+  [
+    { title: 'Board 1' },
+    { title: 'Board 2' },
+    { title: 'Board 3' },
+  ]
+)
+
+columns = boards.map do |board|
+  board.columns.create(
+    [
+      { title: 'Column 1', position: 1 },
+      { title: 'Column 2', position: 2 },
+      { title: 'Column 3', position: 3 },
+    ]
+  )
+end.flatten
+
+columns.each do |column|
+  column.tasks.create(
+    [
+      { title: 'Column 1', position: 1 },
+      { title: 'Column 2', position: 2 },
+      { title: 'Column 3', position: 3 },
+    ]
+  )
+end
+
