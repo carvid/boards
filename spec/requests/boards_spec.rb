3107b939d725941ea4490827e8e984742722f6e6
Carlos Gonzalez <carlosdavid@gonzalez-abraham.com.mx>
2017-10-10 20:34:11 -0500

    Add missing POST /boards/:id/columns spec.
    This commit also improves the rest of the boards endpoints spec examples.

--- spec/requests/boards_spec.rb
+++ spec/requests/boards_spec.rb
@@ -4,6 +4,9 @@ require 'rails_helper'
 require 'requests/schemas'
 
 describe 'Boards endpoints', type: :request do
+  let(:board) { create(:board) }
+  let(:board_id) { board.id }
+
   describe 'GET /boards' do
     let!(:boards) { create_list(:board, 10) }
 
@@ -20,9 +23,7 @@ describe 'Boards endpoints', type: :request do
   end
 
   describe 'GET /boards/:id' do
-    let(:board) { create(:board) }
-
-    before { get "/boards/#{board.id}" }
+    before { get "/boards/#{board_id}" }
 
     context 'when the record exists' do
       it 'returns the board' do
@@ -35,7 +36,7 @@ describe 'Boards endpoints', type: :request do
     end
 
     context 'when the record does not exist' do
-      before { board.destroy }
+      let(:board_id) { -1 }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
@@ -44,10 +45,10 @@ describe 'Boards endpoints', type: :request do
   end
 
   describe 'POST /boards' do
-    let(:request_body) { { title: 'Board 1' } }
+    before { post '/boards', params: request_body }
 
     context 'when the request is valid' do
-      before { post '/boards', params: request_body }
+      let(:request_body) { { title: 'Board 1' } }
 
       it 'creates a board' do
         expect(json).to eq(board_schema)
@@ -59,10 +60,46 @@ describe 'Boards endpoints', type: :request do
     end
 
     context 'when the request is invalid' do
-      before { post '/boards', params: { title: 'Board 1' } }
+      let(:request_body) { { title: nil } }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  describe 'POST /boards/:id/columns' do
+    before { post "/boards/#{board_id}/columns", params: request_body }
+
+    let(:request_body) { {} }
+
+    context 'when the record does not exist' do
+      let(:board_id) { -1 }
+
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
+      end
+    end
+
+    context 'when the board record exists' do
+      context 'when the request is valid' do
+        let(:request_body) { { title: 'Column 1', position: 1 } }
+
+        it 'creates a columns' do
+          expect(json).to eq(column_schema)
+        end
+
+        it 'returns status code 201' do
+          expect(response).to have_http_status(201)
+        end
+      end
+
+      context 'when the request is invalid' do
+        let(:request_body) { { title: 'Column 1' } }
+
+        it 'returns status code 422' do
+          expect(response).to have_http_status(422)
+        end
       end
     end
   end
