23800c57c79233d786f78fc24449024bb15c83b4
urzuae <urzuae@gmail.com>
2017-10-11 13:53:56 -0500

    Add Board component

--- app/javascript/bundles/Workspace/components/Workspace.jsx
+++ app/javascript/bundles/Workspace/components/Workspace.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import BoardSelector from '../containers/board-selector.container';
+import Board from '../components/board';
 
 export default class Workspace extends React.Component {
   render() {
     return (
       <div className="container">
         <BoardSelector />
+        <Board />
       </div>
     );
   }
---
+++ app/javascript/bundles/Workspace/components/board.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="board">
+        <div className="row">
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Board;
