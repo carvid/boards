ec7b01b10bd7a79fbc181873c6a08f3b3e15e5d8
Carlos Gonzalez <carlosdavid@gonzalez-abraham.com.mx>
2017-10-11 17:13:58 -0500

    Set initial redux state from WorkspaceController.

--- app/controllers/workspace_controller.rb
+++ app/controllers/workspace_controller.rb
@@ -3,8 +3,29 @@
 class WorkspaceController < ApplicationController
   layout 'workspace'
 
+  before_action :set_boards
+  before_action :set_current_board
+
   def index
-    @workspace_props = { }
+    @workspace_props = build_workspace_props
+  end
+
+  private
+
+  def set_boards
+    @boards = Board.all
+  end
+
+  def set_current_board
+    @current_board = @boards.first
+  end
+
+  def build_workspace_props
+    {
+      boards: {
+        current: @current_board&.id,
+      },
+    }
   end
 end
 
